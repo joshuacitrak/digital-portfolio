d0dfd13d00d7ed490e1ec626497d8b609f47c167
joshuacitrak <joshua.citrak@apollo.edu>
2015-07-20 15:38:52 -0700

    wired up project, html scaffold

---
+++ config.rb
@@ -0,0 +1,9 @@
+require "susy"
+require "breakpoint"
+css_dir='_/css'
+sass_dir='_/components/sass'
+javascripts_dir='_/components/js'
+output_style= :nested
+relative_assets=true
+add_import_path "bower_components/foundation/scss"
+require 'breakpoint'
