87c5969bcbd1bfdb1b7fac26f609b574b01f1970
joshuacitrak <joshua.citrak@apollo.edu>
2015-07-22 14:06:20 -0700

    masonry, footer

--- _/components/js/app.js
+++ _/components/js/app.js
@@ -1,24 +1,39 @@
 $(document).ready(function () {
-            console.log("document ready");
-            $(".fa-chevron-down").click(chevronClickHandler);
+    console.log("document ready");
+    $(".fa-chevron-down").click(chevronClickHandler);
 
-            function chevronClickHandler(evt) {
-                console.log('clicked');
-                if ($(this).hasClass('fa-rotate-180')) {
-                        $(this).removeClass('fa-rotate-180');
-                    closeBio();
-                    } else {
-                        $(this).addClass('fa-rotate-180');
-                        openBio();
-                    }
-                };//chevronClickHandler
-    
-    function closeBio(){
+    function chevronClickHandler(evt) {
+        console.log('clicked');
+        if ($(this).hasClass('fa-rotate-180')) {
+            $(this).removeClass('fa-rotate-180');
+            closeBio();
+        } else {
+            $(this).addClass('fa-rotate-180');
+            openBio();
+        }
+    }; //chevronClickHandler
+
+    function closeBio() {
         $('.bio-content').removeClass('bio-content-active');
-    };//close bio
-    
-    function openBio(){
+    }; //close bio
+
+    function openBio() {
         $('.bio-content').addClass('bio-content-active');
         console.log("open bio");
-    };//open bio
-            });
+    }; //open bio
+
+    var $masonryContainer = $('.masonry');
+
+    $masonryContainer.imagesLoaded(function () {
+        //might as well remove the spinners here too
+        $('figure').each(function(i){
+            $('.spinner-container').addClass('hidden');
+        });
+        $masonryContainer.masonry({
+            columnWidth: '.projects-column',
+            gutter: '.projects-gutter',
+            itemSelector: '.projects-item',
+            percentPosition: true,
+        });
+    })
+});
