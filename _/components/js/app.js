c08801f117ca9d25c587d24f9566bbdfd01249f6
joshuacitrak <joshua.citrak@apollo.edu>
2015-08-04 11:26:42 -0700

    me sm, xs screen tweaks

--- _/components/js/app.js
+++ _/components/js/app.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
 
-    console.log("document ready");
+    checkScreenSize();
     $(".fa-chevron-down").click(chevronClickHandler);
     $(".projects-item").click(projectsItemClick);
 
     function chevronClickHandler(evt) {
         console.log('clicked');
         if ($(this).hasClass('fa-rotate-180')) {
-            $(this).removeClass('fa-rotate-180');
             closeBio();
         } else {
-            $(this).addClass('fa-rotate-180');
             openBio();
         }
     }; //chevronClickHandler
 
     function closeBio() {
+        $('.fa-chevron-down').removeClass('fa-rotate-180');
         $('.bio-content').removeClass('bio-content-active');
     }; //close bio
 
     function openBio() {
+        $('.fa-chevron-down').addClass('fa-rotate-180');
         $('.bio-content').addClass('bio-content-active');
         console.log("open bio");
     }; //open bio
@@ -35,11 +35,6 @@ $(document).ready(function () {
             body.animate({
                 scrollTop: 0
             }, '300');
-        }
-        
-        if(!$('.fa-chevron-down').hasClass('fa-rotate-180'))
-        {
-            $('.fa-chevron-down').addClass('fa-rotate-180');
         }
     };
     
@@ -66,4 +61,18 @@ $(document).ready(function () {
         nextArrow: ('<i class="fa fa-chevron-right"></i>')
 
     });
+    
+    //window
+    $(window).resize(function(){
+        console.log("resize" + $(window).innerWidth());
+        checkScreenSize();
+    });
+    
+    function checkScreenSize(){
+        var currWidth = $(window).innerWidth();
+        if(currWidth<768)
+        {
+            openBio();
+        }
+    };
 });
